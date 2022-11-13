85639e87f0874fd957784673cfde723d1136fa90
murphyqwek <qwekinaft@list.ru>
2022-11-13 13:48:22 +0700

    Update main window

---
+++ WindowsFormsApp1/Settings.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    static class Settings
+    {
+        static void save(string priority_port, string priority_speed)
+        {
+            string settings = priority_port + "_" + priority_speed;
+
+
+        }
+    }
+}
