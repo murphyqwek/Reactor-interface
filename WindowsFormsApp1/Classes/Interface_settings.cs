08eb3ac21b7c84a9f9622e595354864cd8d2c552
murphyqwek <qwekinaft@list.ru>
2022-11-13 18:01:08 +0700

    Add Interface_settings.cs

---
+++ WindowsFormsApp1/Classes/Interface_settings.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Win32;
+
+namespace WindowsFormsApp1.Classes
+{
+    static class Interface_settings
+    {
+        static public string get_port()
+        {
+            string port;
+            using (RegistryKey key = Registry.CurrentUser.OpenSubKey(@"SOFTWARE\Reactor Interface TPU"))
+            {
+                port = key?.GetValue("Port")?.ToString();
+            }
+            return port;
+        }
+
+        static public int get_speed()
+        {
+            int speed;
+            using (RegistryKey key = Registry.CurrentUser.OpenSubKey(@"SOFTWARE\Reactor Interface TPU"))
+            {
+                speed = Convert.ToInt32(key.GetValue("Speed"));
+            }
+            return speed;
+        }
+
+        static public void save(string port)
+        {
+            using (RegistryKey key = Registry.CurrentUser.CreateSubKey(@"SOFTWARE\Reactor Interface TPU"))
+            {
+                key.SetValue("Port", port);
+            }
+        }
+
+        static public void save(int speed)
+        {
+            using (RegistryKey key = Registry.CurrentUser.CreateSubKey(@"SOFTWARE\Reactor Interface TPU"))
+            {
+                key.SetValue("Speed", speed);
+            }
+        }
+
+        static public void save(string port, int speed)
+        {
+            using (RegistryKey key = Registry.CurrentUser.CreateSubKey(@"SOFTWARE\Reactor Interface TPU"))
+            {
+                key.SetValue("port", port);
+                key.SetValue("Speed", speed);
+            }
+        }
+    }
+}
