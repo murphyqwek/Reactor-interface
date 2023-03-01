d3b00c655882ca5b63f139006e22dde344fabb33
murphyqwek <qwekinaft@list.ru>
2023-03-01 16:05:23 +0700

    Updated

--- WindowsFormsApp1/Classes/GoogleAPI/Auth.cs
+++ WindowsFormsApp1/Classes/GoogleAPI/Auth.cs
@@ -3,10 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Google.Apis;
+using Google.Apis.Drive.v3;
+using Google.Apis.Services;
 
 namespace Reactor_Interface.Classes.GoogleAPI
 {
-    internal class Auth
+    //api_key = AIzaSyBi2-yZOolgSgh5xGlkHmM0w8I6de343CI
+    static class Auth
     {
+        static string api_key = "AIzaSyBi2-yZOolgSgh5xGlkHmM0w8I6de343CI";
+        static DriveService GoogleDriveService = new DriveService(new BaseClientService.Initializer
+        {
+            ApiKey = api_key,
+        });
+
     }
 }
