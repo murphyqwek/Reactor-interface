9625dc06788feafa7ec9d3b2ebea60e6f3f45dab
murphyqwek <qwekinaft@list.ru>
2023-06-22 00:53:45 +0700

    Delete CSVHelper

--- WindowsFormsApp1/Classes/Oscillograph/OscillographParser.cs
+++ WindowsFormsApp1/Classes/Oscillograph/OscillographParser.cs
@@ -7,7 +7,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using CsvHelper;
 using System.Globalization;
 
 namespace Reactor_Interface.Classes.Oscillograph
